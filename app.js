285140fd26e3781dacc3fb00c31d776f6a28764d
Ornella Escalada Corsi <ornellaescaladacorsi@Ornellas-MacBook-Air.fritz.box>
2022-04-27 20:12:42 +0200

    partials

--- app.js
+++ app.js
@@ -13,30 +13,29 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
-// ...
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
 });
-app.get ('/views/beers',(req, res)=>{
-  const beers= punkAPI
-  .getBeers()
-.then(beersFromApi => console.log('Beers from the database: ', beersFromApi))
+app.get ('/beers',(req, res)=>{
+punkAPI
+.getBeers()
+.then(beersFromAPI => {
+res.render('beers', {beersFromAPI});
+})
 .catch(error => console.log(error));
-  res.render('beers', beers);
 });
 
-app.get ('/views/random-beer',(req, res)=>{
-  punkAPI
-  .getRandom()
-  .then(responseFromAPI => {
-    const data=beersFromApi[Math.floor(Math.random()*beersFromApi.length)];
-  })
-  .catch(error => console.log(error));
-  res.render('random-beer', data);
+app.get ('/random-beer',(req, res)=>{
+punkAPI
+.getRandom()
+.then(responseFromAPI => {
+  res.render('random-beer' ,{responseFromAPI});})
+.catch(error => console.log(error)); 
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
